3646c22841c2ce125a9fb7d09a3cb027fd0d30e8
Maya Maddox <hello@mayamaddox.com>
2024-11-22 18:37:46 +0000

    chore: add Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import prettier from "eslint-config-prettier";
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
@@ -7,6 +8,8 @@ export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs["flat/recommended"],
+  prettier,
+  ...svelte.configs['flat/prettier'],
   {
     languageOptions: {
 	  globals: {
