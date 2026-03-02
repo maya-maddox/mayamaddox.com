c37a371f28ef9a7435cb7386eae14245f2e0db33
Maya Maddox <hello@mayamaddox.com>
2026-03-02 21:59:13 +0000

    feat: add hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mayamaddox.com",
+  integrations: [sitemap(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
